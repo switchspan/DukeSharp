304f2528225107fb9dc33eee63393ce4c478d7c1
Ken Taylor <taylor.kenneth@gmail.com>
2012-10-02 15:04:16 -0400

    Added initial library project for duke

---
+++ Duke/Duke/Column.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Duke
+{
+    class Column
+    {
+        private String _name;
+        private String _property;
+        private String _prefix;
+        private ICleaner _cleaner;
+
+        public Column(String name, String property, String prefix, ICleaner cleaner)
+        {
+            _name = name;
+            _property = property;
+            _prefix = prefix;
+            _cleaner = cleaner;
+        }
+
+        public String getName()
+        {
+            return _name;
+        }
+
+        public String getProperty()
+        {
+            if (_property == null)
+            {
+                return _name;
+            }
+            else
+            {
+                return _property;
+            }
+        }
+
+        public String getPrefix()
+        {
+            return _prefix;
+        }
+
+        public ICleaner getCleaner()
+        {
+            return _cleaner;
+        }
+
+
+    }
+}
---
+++ Duke/Duke/ICleaner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Duke
+{
+    /// <summary>
+    /// A function which can turn a value into a normalized value suitable for comparison.
+    /// </summary>
+    interface ICleaner
+    {
+        String Clean(String value);
+    }
+}
