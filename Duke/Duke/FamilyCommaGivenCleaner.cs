72e5ba90f6a6a931d97c2218f128c1300b6a6138
Ken Taylor <taylor.kenneth@gmail.com>
2012-10-02 16:58:14 -0400

    Added another cleaner and StringUtils class

---
+++ Duke/Duke/FamilyCommaGivenCleaner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Duke
+{
+    /// <summary>
+    /// *Experimental* cleaner for person names of the form "Smith,
+    /// John". Based on PersonNameCleaner. It also normalizes periods 
+    /// in initials, so that "J.R. Ackerley" becomes "J. R. Ackerley".
+    /// </summary>
+    public class FamilyCommaGivenCleaner : ICleaner
+    {
+        public string Clean(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Duke/Duke/StringUtils.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Duke
+{
+    public class StringUtils
+    {
+
+        public static String ReplaceAnyOf(String value, String chars, char replacement)
+        {
+            var tmp = new char[value.Length];
+            int pos = 0;
+            foreach (char ch in value)
+            {
+                if (chars.IndexOf(ch) != -1)
+                {
+                    tmp[pos++] = replacement;
+                }
+                else
+                {
+                    tmp[pos++] = ch;
+                }   
+            }
+
+            return tmp.ToString();
+        }
+
+        /// <summary>
+        /// Removes trailing and leading whitespace, and also reduces each 
+        /// sequence of internal whitespace to a single space.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static String NormalizeWs(String value)
+        {
+            //TODO: See if this or the previous implementation in ReplaceAnyOf is faster
+            var tmp = new char[value.Length];
+            int pos = 0;
+            bool prevws = false;
+            for (int ix = 0; ix < tmp.Length; ix++)
+            {
+                var ch = value[ix];
+                if (ch != ' ' && ch != '\t' && ch != '\n' && ch != '\r')
+                {
+                    if (prevws && pos != 0)
+                        tmp[pos++] = ' ';
+
+                    tmp[pos++] = ch;
+                    prevws = false;
+                }
+                else
+                {
+                    prevws = true;
+                }
+            }
+
+            return tmp.ToString();
+        }
+
+
+        public static String[] Split(String str)
+        {
+            var tokens = new String[(str.Length / 2) + 1];
+            int start = 0;
+            int tcount = 0;
+            bool prevws = false;
+            int ix;
+            for (ix = 0; ix < str.Length; ix++)
+            {
+                if (str[ix] == ' ')
+                {
+                    if (!prevws && ix > 0)
+                        tokens[tcount++] = str.Substring(start, ix);
+                    prevws = true;
+                    start = ix + 1;
+                }
+                else
+                {
+                    prevws = false;
+                }
+            }
+
+            if (!prevws && start != ix)
+                tokens[tcount++] = str.Substring(start);
+
+            var tmp = new String[tcount];
+            for (ix = 0; ix < tcount; ix++)
+                tmp[ix] = tokens[ix];
+
+            return tmp;
+        }
+
+        public static String Join(String[] pieces)
+        {
+            var tmp = new StringBuilder();
+            for (int ix = 0; ix < pieces.Length; ix++)
+            {
+                if (ix != 0)
+                    tmp.Append(" ");
+                tmp.Append(pieces[ix]);
+            }
+
+            return tmp.ToString();
+        }
+    }
+}
