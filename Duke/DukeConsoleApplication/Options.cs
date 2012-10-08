eddf93ed1efac07408749a68267aef91f7292bae
switchspan <taylor.kenneth@gmail.com>
2012-10-08 16:40:29 -0400

    Added more options to command line interface.

--- Duke/DukeConsoleApplication/Options.cs
+++ Duke/DukeConsoleApplication/Options.cs
@@ -6,34 +6,34 @@ namespace DukeConsoleApplication
 {
     internal class Options
     {
-        [Option("c", "cfgfile", Required = false, HelpText = "xml configuration file")]
+        [Option("c", "cfgfile", Required = true, HelpText = "XML configuration file.")]
         public string ConfigFile { get; set; }
 
-        [Option("p", "progress", DefaultValue = false, HelpText = "show progress report while running")]
-        public bool Progress { get; set; }
+        [Option("p", "progress", DefaultValue = false, HelpText = "Show progress report while running.")]
+        public bool ShowProgress { get; set; }
 
-        [Option("s", "showmatches", DefaultValue = false, HelpText = "show matches while running")]
+        [Option("s", "showmatches", DefaultValue = false, HelpText = "Show matches while running.")]
         public bool ShowMatches { get; set; }
 
-        [Option("l", "linkfile", Required = false, HelpText = "output matches to link file")]
+        [Option("l", "linkfile", Required = false, HelpText = "Output matches to link file.")]
         public string LinkFile { get; set; }
 
-        [Option("i", "interactive", DefaultValue = false, HelpText = "query user before outputting link file matches")]
-        public bool Interactive { get; set; }
+        [Option("i", "interactive", DefaultValue = false, HelpText = "Query user before outputting link file matches.")]
+        public bool IsInteractive { get; set; }
 
-        [Option("t", "testfile", Required = false, HelpText = "output accuracy stats")]
+        [Option("t", "testfile", Required = false, HelpText = "Output accuracy statistics.")]
         public string TestFile { get; set; }
 
-        [Option("d", "testdebug", DefaultValue = false, HelpText = "display failures")]
-        public bool TestDebug { get; set; }
+        [Option("d", "testdebug", DefaultValue = false, HelpText = "Display failures.")]
+        public bool IsTestDebug { get; set; }
 
-        [Option("v", "verbose", DefaultValue = false, HelpText = "display diagnostics")]
-        public bool Verbose { get; set; }
+        [Option("v", "verbose", DefaultValue = false, HelpText = "Display diagnostics.")]
+        public bool ShowVerbose { get; set; }
 
-        [Option("n", "noreindex", DefaultValue = false, HelpText = "reuse existing Lucene index")]
-        public bool NoReIndex { get; set; }
+        [Option("n", "noreindex", DefaultValue = false, HelpText = "Reuse existing Lucene index.")]
+        public bool IsNoReindex { get; set; }
 
-        [Option("b", "batchsize", DefaultValue = 1, HelpText = "set size of Lucene indexing batches")]
+        [Option("b", "batchsize", DefaultValue = 1, HelpText = "Set size of Lucene indexing batches.")]
         public int BatchSize { get; set; }
 
         [HelpOption(HelpText = "Display this help screen.")]
@@ -49,7 +49,7 @@ namespace DukeConsoleApplication
                                AddDashesToOption = true
                            };
             help.AddPreOptionsLine(padding);
-            help.AddPreOptionsLine("Usage: DukeConsoleApplication.exe -p");
+            help.AddPreOptionsLine("Usage: DukeConsoleApplication.exe -c <cfgfile.xml> [options] ");
             help.AddOptions(this);
 
             return string.Format("{0}{1}{2}", padding, Environment.NewLine, help);
--- Duke/DukeConsoleApplication/Program.cs
+++ Duke/DukeConsoleApplication/Program.cs
@@ -11,17 +11,36 @@ namespace DukeConsoleApplication
     {
         static void Main(string[] args)
         {
+            var linkFile = "NONE";
+            var statsFile = "NONE";
             var options = new Options();
             var parser = new CommandLineParser();
             if (parser.ParseArguments(args, options))
             {
-               
-                Console.WriteLine("Duke Commandline Application 1.0");
-                Console.WriteLine("-----");
-                // consume Options type properties
-                Console.WriteLine(String.Format("Show Progress: {0}", options.Progress));
+                // write the application header
+                Console.WriteLine(GetApplicationHeader());
 
-                
+                Console.WriteLine("Execution Options:");
+                Console.WriteLine(String.Format("Configuration File = {0}", options.ConfigFile));
+
+                // consume Options type properties
+                Console.WriteLine(String.Format("Show ShowProgress = {0}", options.ShowProgress));
+                Console.WriteLine(String.Format("Show Matches = {0}", options.ShowMatches));
+                if (!String.IsNullOrEmpty(options.LinkFile))
+                {
+                    linkFile = options.LinkFile;
+                    Console.WriteLine(String.Format("Link File = {0}", linkFile));
+                }
+                Console.WriteLine(String.Format("IsInteractive = {0}",options.IsInteractive));
+                if (!String.IsNullOrEmpty(options.TestFile))
+                {
+                    statsFile = options.TestFile;
+                    Console.WriteLine(String.Format("Test File = {0}", statsFile));    
+                }
+                Console.WriteLine(String.Format("Verbose = {0}", options.ShowVerbose));
+                Console.WriteLine(String.Format("No Reindex = {0}", options.IsNoReindex));
+                Console.WriteLine(String.Format("Batch Size = {0}", options.BatchSize));
+
             }
             else
             {
@@ -29,7 +48,27 @@ namespace DukeConsoleApplication
                 //Console.WriteLine("Error reading commandline arguments!");
             }
 
-            var keyPressed = Console.ReadKey();
+        }
+
+        private static string GetApplicationHeader()
+        {
+            var sb = new StringBuilder();
+            var padding = "==============================";
+            
+            sb.AppendLine(padding);
+            
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo("Duke Console Application", "1.0"),
+                Copyright = new CopyrightInfo("Ken Taylor", 2012),
+                AdditionalNewLineAfterOption = false,
+                AddDashesToOption = false
+            };
+
+            sb.AppendLine(help);
+            sb.AppendLine(padding);
+
+            return sb.ToString();
         }
     }
 }
